21dd20b7b3d2dc9813242991ec76bb0559a88461
markpeng <mark.peng@uitox.com>
2013-12-22 13:09:44 +0800

    Update shell script.

--- start_report_server.sh
+++ start_report_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /home/uitox/dyn-ip-report-service
-sudo nohup python report-server.py -a 30 -g "mark.peng@uitox.com" -u "mark.peng@uitox.com" -s "Uitox\\!\\!\\)\\$" &
+sudo nohup python report-server.py -a 30 -g "mark.peng@uitox.com" -u "mark.peng@uitox.com" -s "Uitox\\!\\!\\)\\$" -r "markpeng73@gmail.com;alen@uitox.com"  &
 
